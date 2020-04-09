691820f388e84c1ab6ca04300835b56eb8d584b6
Maximilian Mitchell <max@maxis.me>
2020-04-09 17:43:58 +0100

    add cd and git commands

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,12 @@
 # $ visudo
 # github ALL = NOPASSWD: /path/to/deploy.sh
 
+cd $(dirname "$0")
+
+git reset --hard
+git fetch origin
+git checkout master
+git merge $1
+
+# deploy
 docker stack deploy -c <(docker-compose config) notifi-backend
