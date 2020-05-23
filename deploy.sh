b7b779bb9c6b90e9bb7f7d98cf56f04835b62345
Maximilian Mitchell <max@maxis.me>
2020-05-23 17:57:00 +0100

    tidy

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-# for ssh access:
-# $ adduser github
-# $ visudo
-# github ALL = NOPASSWD: /path/to/deploy.sh
-
-cd $(dirname "$0")
-
-git reset --hard
-git fetch origin
-git checkout master
-git merge $1
-
-# deploy
-docker stack deploy -c <(docker-compose config) notifi-backend
+git pull
+export $(grep -v '^#' .env | xargs)
+docker stack deploy -c stack.yml notifi-backend
