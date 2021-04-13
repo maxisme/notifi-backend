fcffd0c9dad63b87d980dce83c69db7065fcfc2f
Maximilian Mitchell <max@max.me.uk>
2021-04-13 15:17:16 +0100

    only use cf for real ip

--- src/main.go
+++ src/main.go
@@ -102,7 +102,7 @@ func main() {
 
 	// middleware
 	var lmt = tollbooth.NewLimiter(maxRequestsPerSecond, &limiter.ExpirableOptions{DefaultExpirationTTL: time.Hour}).SetIPLookups([]string{
-		"Cf-Connecting-Ip", "X-Forwarded-For", "X-Real-IP",
+		"Cf-Connecting-Ip",
 	})
 
 	// HANDLERS
