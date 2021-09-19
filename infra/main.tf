9f365bbfe304c827e2c40815068e3908febdbbbd
Maximilian Mitchell <max@max.me.uk>
2021-09-19 17:08:46 +0100

    tidy

--- infra/main.tf
+++ infra/main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 3.44"
     }
   }
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "notifi"
+    workspaces {
+      name = "github-actions"
+    }
+  }
 }
 
 provider "aws" {
