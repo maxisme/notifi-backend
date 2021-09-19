02d4db7afc2133e4c9164d5298a4037bbff7c1db
Maximilian Mitchell <max@max.me.uk>
2021-09-19 13:06:08 +0100

    tidy

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -1,4 +1,4 @@
-# create notifi repo https://console.aws.amazon.com/ecr/repositories
+# create notifi repo manually - https://console.aws.amazon.com/ecr/repositories
 
 data "aws_ecr_repository" "notifi" {
   name = "notifi"
