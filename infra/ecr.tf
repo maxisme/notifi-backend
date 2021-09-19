929e5d406d570587b3e0ad66b21185cb3bce97c3
Maximilian Mitchell <max@max.me.uk>
2021-09-19 13:00:17 +0100

    tidy

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -1,11 +1,5 @@
-resource "aws_ecr_repository" "notifi" {
-  name                 = "notifi"
-  image_tag_mutability = "MUTABLE"
-}
+# create notifi repo https://console.aws.amazon.com/ecr/repositories
 
 data "aws_ecr_repository" "notifi" {
   name = "notifi"
-  depends_on = [
-    aws_ecr_repository.notifi,
-  ]
 }
