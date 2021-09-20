e3fb83120aae328af0676084b3219b518d3b2975
Maximilian Mitchell <max@max.me.uk>
2021-09-20 22:59:01 +0100

    use go chi proxy

--- src/main.go
+++ src/main.go
@@ -13,8 +13,8 @@ var chiLambda *chiadapter.ChiLambda
 
 func init() {
 	r := chi.NewRouter()
-	r.HandleFunc("*/code", HandleCode)
-	r.HandleFunc("*/api", HandleApi)
+	r.HandleFunc("/prod/code", HandleCode)
+	r.HandleFunc("/prod/api", HandleApi)
 	chiLambda = chiadapter.New(r)
 }
 
