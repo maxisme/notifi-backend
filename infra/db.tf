842dac8f1e418077c00ae99272dce5f5243d62c7
Maximilian Mitchell <max@max.me.uk>
2021-09-21 19:28:35 +0100

    format

--- infra/db.tf
+++ infra/db.tf
@@ -32,24 +32,24 @@ resource "aws_dynamodb_table" "notification-table" {
 }
 
 data "template_file" "policy_notification" {
-  template = "${file("${path.module}/templates/policy.tpl")}"
+  template = file("${path.module}/templates/policy.tpl")
   vars = {
     table_arn = "${aws_dynamodb_table.notification-table.arn}"
   }
 }
 resource "aws_iam_role_policy" "lambda_db_notification_policy" {
-  role = aws_iam_role.iam_for_lambda.id
+  role   = aws_iam_role.iam_for_lambda.id
   policy = data.template_file.policy_notification.rendered
 }
 
 data "template_file" "policy_user" {
-  template = "${file("${path.module}/templates/policy.tpl")}"
+  template = file("${path.module}/templates/policy.tpl")
   vars = {
     table_arn = "${aws_dynamodb_table.user-table.arn}"
   }
 }
 
 resource "aws_iam_role_policy" "lambda_db_user_policy" {
-  role = aws_iam_role.iam_for_lambda.id
+  role   = aws_iam_role.iam_for_lambda.id
   policy = data.template_file.policy_user.rendered
 }
