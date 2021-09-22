c61ca51a01eaa87c65ee69585975e95e94ef3fad
Maximilian Mitchell <max@max.me.uk>
2021-09-22 23:38:50 +0100

    remove connection db

--- infra/db.tf
+++ infra/db.tf
@@ -13,10 +13,21 @@ resource "aws_dynamodb_table" "user-table" {
     type = "S"
   }
 
+  attribute {
+    name = "connection_id"
+    type = "S"
+  }
+
   global_secondary_index {
     name            = "credentials-index"
     projection_type = "ALL"
     hash_key        = "credentials"
+  }
+
+  global_secondary_index {
+    name            = "connection_id-index"
+    projection_type = "ALL"
+    hash_key        = "connection_id"
   }
 }
 
@@ -29,39 +40,6 @@ resource "aws_dynamodb_table" "notification-table" {
     name = "credentials"
     type = "S"
   }
-}
-
-resource "aws_dynamodb_table" "connection-table" {
-  name         = "connection"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "connection_id"
-
-  attribute {
-    name = "connection_id"
-    type = "S"
-  }
-
-  attribute {
-    name = "device_uuid"
-    type = "S"
-  }
-
-  global_secondary_index {
-    name            = "device_uuid-index"
-    projection_type = "ALL"
-    hash_key        = "device_uuid"
-  }
-}
-
-data "template_file" "policy_connection" {
-  template = file("${path.module}/templates/policy.tpl")
-  vars = {
-    table_arn = "${aws_dynamodb_table.connection-table.arn}"
-  }
-}
-resource "aws_iam_role_policy" "lambda_db_ws_policy" {
-  role   = aws_iam_role.iam_for_lambda.id
-  policy = data.template_file.policy_connection.rendered
 }
 
 data "template_file" "policy_notification" {
