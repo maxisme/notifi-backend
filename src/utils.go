68d376affe7583053f3f744a37a1bd696959c8e2
Maximilian Mitchell <max@max.me.uk>
2021-09-25 15:25:51 +0100

    change endpoint

--- src/utils.go
+++ src/utils.go
@@ -119,7 +119,7 @@ func SendWsMessage(requestContext events.APIGatewayWebsocketProxyRequestContext,
 	//https://{api-id}.execute-api.us-east-1.amazonaws.com/{stage}/@connections/{connection_id}
 	// https://execute-api.us-east-1.amazonaws.com/@connections/GN5OCf-coAMCElw%3D
 	endpoint := fmt.Sprintf(
-		"https://%s.execute-api.%s.amazonaws.com/%s",
+		"https://%s.execute-api.%s.amazonaws.com/%s/@connections",
 		requestContext.APIID,
 		Region,
 		requestContext.Stage,
