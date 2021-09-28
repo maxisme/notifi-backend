c117b4ff16520f6418c261ca19141616c7610f76
Maximilian Mitchell <max@max.me.uk>
2021-09-28 22:38:26 +0100

    add double if

--- src/message_handler.go
+++ src/message_handler.go
@@ -11,18 +11,18 @@ import (
 )
 
 func HandleMessage(ctx context.Context, r events.APIGatewayWebsocketProxyRequest) (events.APIGatewayProxyResponse, error) {
+	db, err := GetDB()
+	if err != nil {
+		return WriteError(err, http.StatusInternalServerError)
+	}
+
+	var user User
+	err = db.Table(UserTable).Get("connection_id", r.RequestContext.ConnectionID).Index("connection_id-index").One(&user)
+	if err != nil {
+		return WriteError(err, http.StatusInternalServerError)
+	}
+
 	if r.Body == "." {
-		db, err := GetDB()
-		if err != nil {
-			return WriteError(err, http.StatusInternalServerError)
-		}
-
-		var user User
-		err = db.Table(UserTable).Get("connection_id", r.RequestContext.ConnectionID).Index("connection_id-index").One(&user)
-		if err != nil {
-			return WriteError(err, http.StatusInternalServerError)
-		}
-
 		var notifications []Notification
 		err = db.Table(NotificationTable).Get("credentials", user.Credentials).Index("credentials-index").All(&notifications)
 		if err != nil {
@@ -57,15 +57,10 @@ func HandleMessage(ctx context.Context, r events.APIGatewayWebsocketProxyRequest
 		return WriteError(err, http.StatusBadRequest)
 	}
 
-	db, err := GetDB()
-	if err != nil {
-		return WriteError(err, http.StatusInternalServerError)
-	}
-
 	wtx := db.WriteTx()
 	t := db.Table(NotificationTable)
 	for _, UUID := range uuids {
-		wtx.Delete(t.Delete("uuid", UUID).If("'uuid' = ?", UUID))
+		wtx.Delete(t.Delete("uuid", UUID).If("'uuid' = ?", UUID).If("'credentials' = ?", user.Credentials))
 	}
 	if err := wtx.Run(); err != nil {
 		return WriteError(err, http.StatusInternalServerError)
