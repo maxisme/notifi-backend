50c97ad02034627f585e89aaf082f63b00f93e5b
Maximilian Mitchell <max@max.me.uk>
2021-10-03 22:51:48 +0100

    tidy

--- src/utils.go
+++ src/utils.go
@@ -44,67 +44,6 @@ func WriteEmptySuccess() (events.APIGatewayProxyResponse, error) {
 	}, nil
 }
 
-//const rfc2822 = "Mon, 28 Jan 2013 14:30:00 +0500"
-//
-//type GitHubResponse struct {
-//	TagName     string    `json:"tag_name"`
-//	Name        string    `json:"name"`
-//	Prerelease  bool      `json:"prerelease"`
-//	Draft       bool      `json:"draft"`
-//	CreatedAt   time.Time `json:"created_at"`
-//	PublishedAt time.Time `json:"published_at"`
-//	Assets      []struct {
-//		Name               string `json:"name"`
-//		Size               int    `json:"size"`
-//		BrowserDownloadURL string `json:"browser_download_url"`
-//	} `json:"assets"`
-//	Body string `json:"body"`
-//}
-
-//// RequiredEnvs verifies envKeys all have values
-//func RequiredEnvs(envKeys []string) error {
-//	for _, envKey := range envKeys {
-//		envValue := os.Getenv(envKey)
-//		if envValue == "" {
-//			return fmt.Errorf("missing env variable: '%s'", envKey)
-//		}
-//	}
-//	return nil
-//}
-//
-//// GetGitHubResponses parses json from http response
-//func GetGitHubResponses(url string) ([]GitHubResponse, error) {
-//	const cacheKey = "github-response"
-//
-//	if githubResponses, found := c.Get(cacheKey); found {
-//		return githubResponses.([]GitHubResponse), nil
-//	}
-//
-//	var client = &http.Client{Timeout: 2 * time.Second}
-//	r, err := client.Get(url)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	defer r.Body.Close()
-//
-//	var githubResponses []GitHubResponse
-//	err = json.NewDecoder(r.Body).Decode(&githubResponses)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	err = c.Add(cacheKey, githubResponses, cache.DefaultExpiration)
-//	if err != nil {
-//		return nil, err
-//	}
-//	return githubResponses, err
-//}
-//
-//func GetWSChannelKey(channel string) string {
-//	return crypt.Hash(channel)
-//}
-
 func SendWsMessage(connectionID string, msgData []byte) error {
 	connectionInput := &apigatewaymanagementapi.PostToConnectionInput{
 		ConnectionId: aws.String(connectionID),
