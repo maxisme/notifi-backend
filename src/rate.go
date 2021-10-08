aff003e21dfaf1fb16ba3f31318e2c4ce0b3fd98
Maximilian Mitchell <max@max.me.uk>
2021-10-08 22:08:38 +0100

    make

--- src/rate.go
+++ src/rate.go
@@ -18,10 +18,10 @@ type localCounter struct {
 
 var bruteForceTable = os.Getenv("BRUTE_FORCE_TABLE_NAME")
 
-type count struct {
-	key       uint64    `dynamo:"brute_key,hash"`
-	value     int       `dynamo:"value"`
-	updatedAt time.Time `dynamo:"updated_dttm"`
+type Count struct {
+	Key       uint64    `dynamo:"brute_key,hash"`
+	Value     int       `dynamo:"value"`
+	UpdatedAt time.Time `dynamo:"updated_dttm"`
 }
 
 func getLimitCounter(db *dynamo.DB) httprate.LimitCounter {
@@ -30,11 +30,11 @@ func getLimitCounter(db *dynamo.DB) httprate.LimitCounter {
 
 var _ httprate.LimitCounter = &localCounter{}
 
-func (c *localCounter) getCounter(key string, window time.Time) *count {
+func (c *localCounter) getCounter(key string, window time.Time) *Count {
 	k := httprate.LimitCounterKey(key, window)
-	var cnt count
-	if err := c.db.Table(bruteForceTable).Get("brute_key", k).One(&cnt); err != nil || cnt.key == 0 {
-		return &count{key: k, value: 0, updatedAt: time.Now()}
+	var cnt Count
+	if err := c.db.Table(bruteForceTable).Get("brute_key", k).One(&cnt); err != nil || cnt.Key == 0 {
+		return &Count{Key: k, Value: 0, UpdatedAt: time.Now()}
 	}
 	return &cnt
 }
@@ -48,7 +48,7 @@ func (c *localCounter) Increment(key string, currentWindow time.Time) error {
 	defer c.mu.Unlock()
 
 	v := c.getCounter(key, currentWindow)
-	v.value += 1
+	v.Value += 1
 
 	fmt.Printf("%+v\n", v)
 	if err := c.db.Table(bruteForceTable).Put(&v).Run(); err != nil {
@@ -65,7 +65,7 @@ func (c *localCounter) Get(key string, currentWindow, previousWindow time.Time) 
 	curr := c.getCounter(key, currentWindow)
 	prev := c.getCounter(key, previousWindow)
 
-	return curr.value, prev.value, nil
+	return curr.Value, prev.Value, nil
 }
 
 func (c *localCounter) evict() error {
@@ -78,14 +78,14 @@ func (c *localCounter) evict() error {
 		return nil
 	}
 
-	var counters []count
+	var counters []Count
 	if err := c.db.Table(bruteForceTable).Scan().Filter("'updated_dttm' >= ?", time.Now().Add(-d)).All(&counters); err != nil {
 		return err
 	}
 
 	var keys []dynamo.Keyed
 	for _, v := range counters {
-		keys = append(keys, dynamo.Keys{v.key, v.updatedAt})
+		keys = append(keys, dynamo.Keys{v.Key, v.UpdatedAt})
 	}
 	_, err := c.db.Table(bruteForceTable).Batch("brute_key", "updated_dttm").Write().Delete(keys...).Run()
 	return err
