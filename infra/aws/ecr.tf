3f1f4a2bb1620b907e56f653dab7cde6d14384ce
Maximilian Mitchell <max@paradime.io>
2024-09-13 17:24:53 +0100

    add hash tagging

--- infra/aws/ecr.tf
+++ infra/aws/ecr.tf
@@ -18,6 +18,18 @@ resource "aws_ecr_lifecycle_policy" "notifi" {
             "action": {
                 "type": "expire"
             }
+        },
+        {
+            "rulePriority": 2,
+            "description": "Keep only the 5 most recent tagged images, expire older ones",
+            "selection": {
+                "tagStatus": "tagged",
+                "countType": "imageCountMoreThan",
+                "countNumber": 5
+            },
+            "action": {
+                "type": "expire"
+            }
         }
     ]
 }
