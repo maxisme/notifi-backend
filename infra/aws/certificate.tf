eaa099f29e152b88d0f39dca243b855c1065595f
Maximilian Mitchell <max@paradime.io>
2024-09-13 18:17:49 +0100

    fix certs

--- infra/aws/certificate.tf
+++ infra/aws/certificate.tf
@@ -1,7 +1,9 @@
 resource "aws_acm_certificate_validation" "notifi" {
-  certificate_arn = aws_acm_certificate.notifi.arn
+  count           = var.IS_DEV ? 0 : 1
+  certificate_arn = aws_acm_certificate.notifi[0].arn
 }
 resource "aws_acm_certificate" "notifi" {
+  count             = var.IS_DEV ? 0 : 1
   domain_name       = var.CF_DOMAIN
   validation_method = "EMAIL"
 }
